e3b0b46c138f43f338bc96819801149ad3df1003
Ray McClain <rmcclain@decibelinsight.com>
2018-07-19 15:44:34 -0600

    Fixed issue with node modules

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
+var path = require('path');
+var fs = require('fs');
+
 function aliasImporter (alias) {
 
-    function runAlias (key, alias, url) {
-        var index = url.indexOf(alias);
-        
-        if(index > -1){
-            url = url.substr(0, index) + key + url.substr(index + alias.length, url.length);
-            runAlias(key, alias, url);
-        }
+	function runAlias (key, alias, url) {
+		var index = url.indexOf(alias);
+		
+		if(index > -1) {
+			url = url.substr(0, index) + key + url.substr(index + alias.length, url.length);
+			runAlias(key, alias, url);
+		}
  
-        return url;
-    }
+		return url;
+	}
  
-    return function importer (url, prev) {
-        for(var k in alias){
-            url = runAlias(k, alias[k], url);
-        }
+	return function importer (url) {
+		for(var k in alias) {
+			url = runAlias(k, alias[k], url);
+		}
 
-        return {contents: fs.readFileSync(url).toString()};
-    }
+		try {
+			return {contents: fs.readFileSync(path.resolve(url)).toString()};
+		} catch (e) {
+			console.log('[node-sass-importer-alias] Error', e);
+		}
+	}
 }
 
 exports.default = aliasImporter;
